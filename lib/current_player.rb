af1dc11250fa75451156edcbb97122dcb9d097b5
Tomislav Stamenovski <krypton.style@gmail.com>
2018-04-29 09:14:51 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,24 +1,5 @@
 def turn_count(arr)
   counter = 0
-  # arr.each do |char|
-  #   if char == "X" || char == "O"
-  #     counter += 1
-  #   end
-  # end
-
-  # arr.each do |char|
-  #   ( char == "X" || char == "O" ) ? counter += 1 : counter += 0
-  # end
-
-  # arr.each do |char|
-  #   ( char == "X" || char == "O" ) ? counter += 1 : nil
-  # end
-
-  # arr.each do |char|
-  #   char == "X" || char == "O" ? counter += 1 : nil
-  # end
-
   arr.each { |char| char == "X" || char == "O" ? counter += 1 : nil }
-
   counter
 end

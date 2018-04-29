222226522cca5e87d20fbb3744af27c8a40757b9
Tomislav Stamenovski <krypton.style@gmail.com>
2018-04-29 09:38:33 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -5,5 +5,5 @@ def turn_count(arr)
 end
 
 def current_player(arr)
-  turn_count(arr) == 0 ? "X" : turn_count(arr) % 2 == 0 ? "X" : "O"
+  turn_count(arr) == 0 ? "X" : ( turn_count(arr) % 2 == 0 ? "X" : "O" )
 end
